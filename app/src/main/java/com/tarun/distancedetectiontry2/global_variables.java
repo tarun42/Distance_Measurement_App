f593870886141bc311dfd8dfe0dce988fe2d81ab
tarun42 <dastarun42@gmail.com>
2020-08-30 00:06:10 +0530

    added variables

--- app/src/main/java/com/tarun/distancedetectiontry2/global_variables.java
+++ app/src/main/java/com/tarun/distancedetectiontry2/global_variables.java
@@ -4,7 +4,7 @@ import android.app.Application;
 
 public class global_variables extends Application {
 
-    public static int a=0,b=0,c=0,d=0,e=0,f=0;
+    public static int a=0,b=0,c=0,d=0,e=0,f=0,BT=0;
 
     public static int getA() {
         return a;
@@ -28,5 +28,8 @@ public class global_variables extends Application {
 
     public static int getF() {
         return f;
+    }
+    public static int getBT() {
+        return BT;
     }
 }
