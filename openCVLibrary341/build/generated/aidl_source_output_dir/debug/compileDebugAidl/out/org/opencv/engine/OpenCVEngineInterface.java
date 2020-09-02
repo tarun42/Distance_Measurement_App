1a12cea63b43e355ce2d394f549b5873e9e892de
tarun42 <dastarun42@gmail.com>
2020-09-02 19:54:14 +0530

    changed buildSDKversion

--- openCVLibrary341/build/generated/aidl_source_output_dir/debug/compileDebugAidl/out/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary341/build/generated/aidl_source_output_dir/debug/compileDebugAidl/out/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: D:\\Tarun_PC\\B\\studis\\ANDROID_ALL_PROJECT\\distancedetectiontry2\\openCVLibrary341\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: /home/tarun/Distance_Measurement_App/openCVLibrary341/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
